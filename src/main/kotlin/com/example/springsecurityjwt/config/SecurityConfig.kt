6181c1928a7065c5425b6460c1255a02df02b86a
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2021-06-05 17:52:14 -0300

    Improving security config

--- src/main/kotlin/com/example/springsecurityjwt/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/springsecurityjwt/config/SecurityConfig.kt
@@ -1,19 +1,50 @@
 package com.example.springsecurityjwt.config
 
+import com.example.springsecurityjwt.filters.AuthenticationFilter
+import com.example.springsecurityjwt.filters.AuthorizationFilter
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpMethod.POST
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy.STATELESS
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @EnableWebSecurity
-class SecurityConfig : WebSecurityConfigurerAdapter() {
+class SecurityConfig(
+    private val mapper: ObjectMapper,
+    private val userDetailsService: UserDetailsService,
+    private val passwordEncoder: PasswordEncoder
+) : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
         http.cors().and().csrf().disable()
             .authorizeRequests()
-            .anyRequest().permitAll()
+            .antMatchers(POST, "/security/**").permitAll()
+            .antMatchers(
+                "/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**",
+                "/v3/api-docs/**",
+                "/swagger-ui/**"
+            ).permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .addFilter(
+                AuthenticationFilter(authenticationManager(), mapper).apply {
+                    setFilterProcessesUrl("/security/log-in")
+                }
+            )
+            .addFilter(AuthorizationFilter(authenticationManager()))
+            .sessionManagement().sessionCreationPolicy(STATELESS)
     }
 
     @Bean
@@ -22,5 +53,9 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
             "/**",
             CorsConfiguration().applyPermitDefaultValues()
         )
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder)
     }
 }
