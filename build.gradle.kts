59fda15259874ba0216184bf3d2c1cb166e12846
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2021-06-06 13:33:03 -0300

    Formatting build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 object DependencyVersion {
-	const val OPEN_API = "1.5.9"
-	const val JJWT = "0.11.2"
+    const val OPEN_API = "1.5.9"
+    const val JJWT = "0.11.2"
 }
 
 plugins {
-	id("org.springframework.boot") version "2.5.0"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	id("org.asciidoctor.convert") version "1.5.8"
-	kotlin("jvm") version "1.5.10"
-	kotlin("plugin.spring") version "1.5.10"
-	kotlin("plugin.jpa") version "1.5.10"
+    id("org.springframework.boot") version "2.5.0"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.asciidoctor.convert") version "1.5.8"
+    kotlin("jvm") version "1.5.10"
+    kotlin("plugin.spring") version "1.5.10"
+    kotlin("plugin.jpa") version "1.5.10"
 }
 
 group = "com.example"
@@ -19,16 +19,16 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
-	all {
-		exclude(module = "spring-boot-starter-logging")
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+    all {
+        exclude(module = "spring-boot-starter-logging")
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 val snippetsDir by extra { file("build/generated-snippets") }
@@ -36,101 +36,102 @@ val ktlint by configurations.creating
 extra["springCloudVersion"] = "2020.0.3"
 
 dependencies {
-	// Spring
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-batch")
-	implementation("org.springframework.boot:spring-boot-starter-cache")
-	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-data-rest")
-	implementation("org.springframework.boot:spring-boot-starter-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-web-services")
-	implementation("org.springframework.boot:spring-boot-starter-log4j2")
+    // Spring
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-batch")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-rest")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web-services")
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
 
-	// Mapper
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    // Mapper
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-	// Kotlin
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-	// JWT
-	implementation("io.jsonwebtoken:jjwt-impl:${DependencyVersion.JJWT}")
-	implementation("io.jsonwebtoken:jjwt-api:${DependencyVersion.JJWT}")
-	implementation("io.jsonwebtoken:jjwt-jackson:${DependencyVersion.JJWT}")
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-impl:${DependencyVersion.JJWT}")
+    implementation("io.jsonwebtoken:jjwt-api:${DependencyVersion.JJWT}")
+    implementation("io.jsonwebtoken:jjwt-jackson:${DependencyVersion.JJWT}")
 
-	// Swagger
-	implementation("org.springdoc:springdoc-openapi-ui:${DependencyVersion.OPEN_API}")
-	implementation("org.springdoc:springdoc-openapi-data-rest:${DependencyVersion.OPEN_API}")
-	implementation("org.springdoc:springdoc-openapi-kotlin:${DependencyVersion.OPEN_API}")
+    // Swagger
+    implementation("org.springdoc:springdoc-openapi-ui:${DependencyVersion.OPEN_API}")
+    implementation("org.springdoc:springdoc-openapi-data-rest:${DependencyVersion.OPEN_API}")
+    implementation("org.springdoc:springdoc-openapi-kotlin:${DependencyVersion.OPEN_API}")
+    implementation("org.springdoc:springdoc-openapi-security:${DependencyVersion.OPEN_API}")
 
-	// Dev Tools
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
+    // Dev Tools
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-	// DB Drivers
-	runtimeOnly("com.h2database:h2")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    // DB Drivers
+    runtimeOnly("com.h2database:h2")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-	// Test
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.batch:spring-batch-test")
-	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
-	testImplementation("org.springframework.security:spring-security-test")
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.batch:spring-batch-test")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    testImplementation("org.springframework.security:spring-security-test")
 
-	// Linter
-	ktlint("com.pinterest:ktlint:0.41.0")
+    // Linter
+    ktlint("com.pinterest:ktlint:0.41.0")
 }
 
 val outputDir = "${project.buildDir}/reports/ktlint/"
 val inputFiles = project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt"))
 
 val ktlintCheck by tasks.creating(JavaExec::class) {
-	inputs.files(inputFiles)
-	outputs.dir(outputDir)
+    inputs.files(inputFiles)
+    outputs.dir(outputDir)
 
-	description = "Check Kotlin code style."
-	classpath = ktlint
-	main = "com.pinterest.ktlint.Main"
-	args = listOf("src/**/*.kt")
+    description = "Check Kotlin code style."
+    classpath = ktlint
+    main = "com.pinterest.ktlint.Main"
+    args = listOf("src/**/*.kt")
 }
 
 val ktlintFormat by tasks.creating(JavaExec::class) {
-	inputs.files(inputFiles)
-	outputs.dir(outputDir)
+    inputs.files(inputFiles)
+    outputs.dir(outputDir)
 
-	description = "Fix Kotlin code style deviations."
-	classpath = ktlint
-	main = "com.pinterest.ktlint.Main"
-	args = listOf("-F", "src/**/*.kt")
+    description = "Fix Kotlin code style deviations."
+    classpath = ktlint
+    main = "com.pinterest.ktlint.Main"
+    args = listOf("-F", "src/**/*.kt")
 }
 
 dependencyManagement {
-	imports {
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-	}
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 
-	finalizedBy(ktlintCheck)
+    finalizedBy(ktlintCheck)
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.test {
-	outputs.dir(snippetsDir)
+    outputs.dir(snippetsDir)
 }
 
 tasks.asciidoctor {
-	inputs.dir(snippetsDir)
-	dependsOn(tasks.test)
+    inputs.dir(snippetsDir)
+    dependsOn(tasks.test)
 }
